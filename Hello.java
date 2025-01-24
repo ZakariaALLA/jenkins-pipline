4e43082e800645518da161e6ae3b27c11c3cf038
Zakaria ALLA <z.alla@mundiapolis.ma>
2025-01-24 23:21:22 +0100

    3rd Line to trigger jenkins

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ class Hello {
     public static void main(String[] args) {
         System.out.println("Hello from Java Program.");
         System.out.println("Second line to trigger jenkins.");
+        System.out.println("Third line to trigger jenkins.");
     }
 }
